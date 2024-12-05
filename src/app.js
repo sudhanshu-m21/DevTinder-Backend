a04a20a95be055647f4d6750edf22136b803b84d
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2024-12-05 11:55:26 +0530

    connected to server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res) => {
+  res.send("hello from vs code");
+});
+app.listen(3000, () => {
+  console.log("server started");
+});
