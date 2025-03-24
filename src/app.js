b01e084883f71491e0f522d61a924f9160e2a006
sudhanshu-m21 <139671253+sudhanshu-m21@users.noreply.github.com>
2025-03-24 11:36:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://devtinder-frontend-1syb.onrender.com",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
   })
